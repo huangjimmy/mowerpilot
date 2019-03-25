32261e4517c3abdc9b768edec3b055ca99899de6
huangsj <jimmy.s.huang@gmail.com>
2019-03-25 18:31:10 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jimmy on 2019/3/25.
+//
+
+// A simple program that computes the square root of a number
+
+#include <AP_NavEKF2/AP_NavEKF2.h>
+
+int main (int argc, char *argv[])
+{
+    NavEKF2 navEKF2;
+    return 0;
+}
